6e8fb360ad8746e86c4e1d876c00c2f20b07c04f
josimarviana <josimarviana@gmail.com>
2022-09-23 18:44:40 -0300

    ajustes na Main

--- src/main/java/br/com/appdahora/javafxaddressapp/controller/MainApp.java
+++ src/main/java/br/com/appdahora/javafxaddressapp/controller/MainApp.java
@@ -5,11 +5,15 @@ import br.com.appdahora.javafxaddressapp.model.Person;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 
 public class MainApp extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
     private ObservableList<Person> personData = FXCollections.observableArrayList();
     public MainApp() {
 // Insira dados de exemplo
@@ -18,7 +22,26 @@ public class MainApp extends Application {
         personData.add(new Person("Francisco", "Pereira"));
     }
 
+    public void showPersonOverview() {
+        try {
+            // Carrega a person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp
+                    .class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview=(AnchorPane) loader.load();
+            // Define a person overview no centro do root layout.
+            rootLayout.setCenter(personOverview);
+            // Dá ao controlador acesso à the main app.
+            PersonOverviewController controller
+                    = loader.getController();
+            controller.setMainApp(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public ObservableList<Person> getPersonData() {
+
         return personData;
     }
     public static void main(String[] args) {
@@ -26,11 +49,32 @@ public class MainApp extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("view/PersonOverview.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Address App!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showPersonOverview();
     }
+
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
 }
